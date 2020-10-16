1332557b27c5737cd6418bbf0d9c64b4a0e29219
Jesse Hill <j_hill14@u.pacific.edu>
2020-10-15 21:25:08 -0400

    remove commented code

--- react-set-builder/src/App.js
+++ react-set-builder/src/App.js
@@ -55,33 +55,7 @@ function App() {
       .attr("r", "4")
       .style("fill", "blue")
       .attr("stroke", "black");
-
-    // svg.selectAll(".bar")
-    //   .data(data)
-    //   .join("rect")
-    //   .attr("class", "bar")
-    //   .style("transform", "scale(1, -1)")
-    //   .attr("x", (value, index) => xScale(index))
-    //   .attr("y", -150)
-    //   .attr("width", xScale.bandwidth())
-    //   .on("mouseenter", function(event, value) {
-    //     const index = svg.selectAll(".bar").nodes().indexOf(this);
-    //     svg.selectAll(".tooltip")
-    //       .data([value])
-    //       .join((enter) => enter.append("text").attr("y", yScale(value) - 4))
-    //       .attr("class", "tooltip")
-    //       .text(value)
-    //       .attr("x", xScale(index))
-    //       .transition()
-    //       .attr("y", yScale(value) -8)
-    //       .attr("opacity", 1);
-    //   })
-    //   .on("mouseleave", () => svg.select(".tooltip").remove())
-    //   .transition()
-    //   .attr("fill", colorScale)
-    //   .attr("height", value => 150 - yScale(value));
-
-    
+   
     }, [data])
 
   return (
