776e57fdfc8d8c30e4a584cb01adc562d363cf74
Jesse Hill <j_hill14@u.pacific.edu>
2020-10-15 21:17:01 -0400

    lollipop chart

--- react-set-builder/src/App.js
+++ react-set-builder/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import logo from './logo.svg';
-import {select, line, curveCardinal, axisBottom, axisRight, scaleLinear } from "d3";
+import {select, axisBottom, axisRight, scaleLinear, scaleBand } from "d3";
 import './App.css';
 
 
@@ -10,43 +10,81 @@ function App() {
   const svgRef = useRef();
   useEffect(() => {
     const svg = select(svgRef.current);
-
-    const xScale = scaleLinear()
-      .domain([0, data.length-1])
+    const xScale = scaleBand()
+      .domain(data.map((value, index) => index))
       .range([0, 300])
-
+      .padding(0.5);
 
     const yScale = scaleLinear()
-      .domain([0, 75])
-      .range([150, 0]) 
+      .domain([0, 100])
+      .range([150, 0]);
+
+    const colorScale = scaleLinear()
+      .domain([75, 150])
+      .range(["green", "red"])
+      .clamp(true);
 
     const xAxis = axisBottom(xScale)
       .ticks(data.length)
-      .tickFormat(index => index + 1);
     svg.select(".x-axis")
       .style("transform", "translateY(150px)")
       .call(xAxis);
-
-    const myLine = line()
-      .x((value, index) => xScale(index))
-      .y(yScale)
-      .curve(curveCardinal);
-
 
     const yAxis = axisRight(yScale);
     svg
       .select(".y-axis")
       .style("transform", "translateX(300px)")
       .call(yAxis)
-    
 
-    svg.selectAll(".line")
-      .data([data])
-      .join("path")
-      .attr("class", "line")
-      .attr("d", myLine)
-      .attr("fill", "none")
+    svg.selectAll("myline")
+      .data(data)
+      .enter()
+      .append("line")
+      .attr("x1", (value, index) => xScale(index))
+      .attr("x2", (value, index) => xScale(index))
+      .attr("y1", (value, index) => {
+        console.log(yScale(value));
+        return yScale(value)
+      })
+      .attr("y2", yScale(0))
       .attr("stroke", "red");
+
+    svg.selectAll("myCircle")
+      .data(data)
+      .enter()
+      .append("circle")
+      .attr("cx", (value, index) => xScale(index))
+      .attr("cy", (value, index) => yScale(value))
+      .attr("r", "4")
+      .style("fill", "blue")
+      .attr("stroke", "black");
+
+    // svg.selectAll(".bar")
+    //   .data(data)
+    //   .join("rect")
+    //   .attr("class", "bar")
+    //   .style("transform", "scale(1, -1)")
+    //   .attr("x", (value, index) => xScale(index))
+    //   .attr("y", -150)
+    //   .attr("width", xScale.bandwidth())
+    //   .on("mouseenter", function(event, value) {
+    //     const index = svg.selectAll(".bar").nodes().indexOf(this);
+    //     svg.selectAll(".tooltip")
+    //       .data([value])
+    //       .join((enter) => enter.append("text").attr("y", yScale(value) - 4))
+    //       .attr("class", "tooltip")
+    //       .text(value)
+    //       .attr("x", xScale(index))
+    //       .transition()
+    //       .attr("y", yScale(value) -8)
+    //       .attr("opacity", 1);
+    //   })
+    //   .on("mouseleave", () => svg.select(".tooltip").remove())
+    //   .transition()
+    //   .attr("fill", colorScale)
+    //   .attr("height", value => 150 - yScale(value));
+
+    
     }, [data])
 
   return (
@@ -64,6 +102,9 @@ function App() {
       </button>
       <button onClick={() => setData(data.filter(value => value <= 35))}> 
         Filter data
+      </button>
+      <button onClick={() => setData([...data, Math.round(Math.random()*100)])}> 
+        Add data
       </button>
     </React.Fragment>
   );
