7444e1fe45154ce7d423b05d9fc922e46d43013d
Jesse Hill <j_hill14@u.pacific.edu>
2020-10-20 13:47:21 -0400

    new lolipop component

--- react-set-builder/src/App.js
+++ react-set-builder/src/App.js
@@ -6,6 +6,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 // import Column from 'react-bootstrap/CardColumns';
 import Column from 'react-bootstrap/Col';
+import Lolipop from './Lolipop';
 
 
 const MARGIN = { LEFT: 100, RIGHT: 10, TOP: 50, BOTTOM: 130};
@@ -23,107 +24,6 @@ function App() {
     console.log("Generate");
     console.log(data);
   }
-
-  const svgRef = useRef();
-  useEffect(() => {
-    // const svg = select(svgRef.current)
-    const svg = select("#chart-area").append("svg")
-      // .attr("viewBox", [0, 0, 500, 500]);
-      .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
-      .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
-    
-    const g = svg.append("g")
-      // .attr("transform",`translate(${MARGIN.LEFT}, ${MARGIN.TOP})`)
-
-    const xScale = scaleBand()
-      .domain(data.map((d) => d.id))
-      .range([0, 300])
-      .padding(0.5);
-
-    const yScale = scaleLinear()
-      .domain([0, 100])
-      .range([100, 0]);
-
-    const xAxis = axisBottom(xScale)
-      .ticks(data.length)
-    // svg.select(".x-axis")
-    //   .style("transform", `translateY(${(HEIGHT - MARGIN.TOP - MARGIN.BOTTOM)/2}px)`)
-    //   .call(xAxis);
-
-    const yAxis = axisRight(yScale);
-    svg
-      .select(".y-axis")
-      // .style("transform", `translateY(${(WIDTH - MARGIN.LEFT - MARGIN.RIGHT)/2}px)`)
-      .attr("align", "center")
-      .call(yAxis)
-    // const drg = drag().on("start", dragstart).on("drag", dragged);
-
-    const node = g
-      .selectAll(".node")
-      .data(data)
-      .join("circle")
-      .attr("r", 12)
-      .attr("class", "node")
-      // .attr("fixed", true)
-
-
-    const link = g
-      .selectAll(".stick")
-      .data(data)
-      .join("line")
-      .attr("class", "stick")
-      .attr("stroke", "grey")
-      // .classed("node", true)
-      // .classed("fixed", d => d.value !== undefined);
-
-    const simulation = forceSimulation()
-      .nodes(data)
-      .force("charge", forceManyBody())
-      // .force("center", forceCenter(100, 50))
-      .on("tick", tick);
-    
-    const drg = drag()
-      .on("start", dragStart)
-      .on("drag", dragged);
-
-    node.call(drg).on("click", click);
-
-    function tick(){
-        node
-          .attr("cx", (d) => xScale(d.id))
-          .attr("cy", (d) => yScale(d.value))
-          .attr("r", 4)
-        link
-          .attr("x1", (d) => xScale(d.id))
-          .attr("y2", (d) => yScale(d.value)+6)
-          .attr("x2", (d) => xScale(d.id))
-          .attr("y1", (d) => yScale(0))
-        }
-
-      
-    function clamp(x, lo, hi){
-      return x < lo ? lo : x > hi ? hi : x;
-    }
-
-    function click(event, d){
-      delete d.fy;
-      select(this)
-        .attr("fixed", true);
-      simulation.alpha(1).restart();
-    }
-
-    function dragStart(){
-      select(this)
-        .attr("fixed", true)
-    }
-
-    function dragged(event, d) {
-      d.value = clamp(d.value-event.dy, 10, 100)
-      simulation.alpha(1).restart();
-    }
-
-    }, [data])
-
   return (
     <React.Fragment>
       <Container id='view-area'>
@@ -132,9 +32,7 @@ function App() {
             <h1> Col 1</h1>
           </Column>
           <Column md={7} id='chart-col'>
-          <div id="chart-area">
-
-          </div>
+            <Lolipop data={data} />
           </Column>
           <Column id='right-col'>
             <h1> Col 3</h1>
---
+++ react-set-builder/src/Lolipop.js
@@ -0,0 +1,147 @@
+import React, { useState, useRef, useEffect } from 'react';
+import logo from './logo.svg';
+import {select, axisBottom, axisRight, scaleLinear, scaleBand, drag, keys, forceSimulation, create, forceManyBody, forceCenter} from "d3";
+import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+// import Column from 'react-bootstrap/CardColumns';
+import Column from 'react-bootstrap/Col';
+import ResizeObserver from "resize-observer-polyfill";
+
+const MARGIN = { LEFT: 100, RIGHT: 10, TOP: 50, BOTTOM: 130};
+const WIDTH = 600 - MARGIN.LEFT - MARGIN.RIGHT;
+const HEIGHT = 500 - MARGIN.TOP - MARGIN.BOTTOM;
+
+// const useResizeObserver = (ref) => {
+//     const [dimensions, setDimensions] = useState(null);
+//     useEffect(() => {
+//         const observeTarget = ref.current;
+//         const resizeObserver = new resizeObserver((entries) => {
+//             entries.forEach((entry) => {
+//                 setDimensions(entry.contentRect);
+//             });
+//         });
+//         resizeObserver.observe(observeTarget);
+//         return () => {
+//             resizeObserver.unobserve(observeTarget);
+//         };
+
+//     }, [ref]);
+//     return dimensions;
+// }
+
+function Lolipop({data}) {
+
+    const svgRef = useRef();
+    const wrapperRef = useRef();
+    // const dimensions = useResizeObserver(wrapperRef);
+
+
+    useEffect(() => {
+        // const svg = select(svgRef.current)
+        const svg = select("#chart-area").append("svg")
+          // .attr("viewBox", [0, 0, 500, 500]);
+          .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
+          .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
+        
+        const g = svg.append("g")
+          // .attr("transform",`translate(${MARGIN.LEFT}, ${MARGIN.TOP})`)
+    
+        const xScale = scaleBand()
+          .domain(data.map((d) => d.id))
+          .range([0, 300])
+          .padding(0.5);
+    
+        const yScale = scaleLinear()
+          .domain([0, 100])
+          .range([100, 0]);
+    
+        const xAxis = axisBottom(xScale)
+          .ticks(data.length)
+        // svg.select(".x-axis")
+        //   .style("transform", `translateY(${(HEIGHT - MARGIN.TOP - MARGIN.BOTTOM)/2}px)`)
+        //   .call(xAxis);
+    
+        const yAxis = axisRight(yScale);
+        svg
+          .select(".y-axis")
+          // .style("transform", `translateY(${(WIDTH - MARGIN.LEFT - MARGIN.RIGHT)/2}px)`)
+          .attr("align", "center")
+          .call(yAxis)
+        // const drg = drag().on("start", dragstart).on("drag", dragged);
+    
+        const node = g
+          .selectAll(".node")
+          .data(data)
+          .join("circle")
+          .attr("r", 12)
+          .attr("class", "node")
+          // .attr("fixed", true)
+    
+    
+        const link = g
+          .selectAll(".stick")
+          .data(data)
+          .join("line")
+          .attr("class", "stick")
+          .attr("stroke", "grey")
+          // .classed("node", true)
+          // .classed("fixed", d => d.value !== undefined);
+    
+        const simulation = forceSimulation()
+          .nodes(data)
+          .force("charge", forceManyBody())
+          // .force("center", forceCenter(100, 50))
+          .on("tick", tick);
+        
+        const drg = drag()
+          .on("start", dragStart)
+          .on("drag", dragged);
+    
+        node.call(drg).on("click", click);
+    
+        function tick(){
+            node
+              .attr("cx", (d) => xScale(d.id))
+              .attr("cy", (d) => yScale(d.value))
+              .attr("r", 4)
+            link
+              .attr("x1", (d) => xScale(d.id))
+              .attr("y2", (d) => yScale(d.value)+6)
+              .attr("x2", (d) => xScale(d.id))
+              .attr("y1", (d) => yScale(0))
+            }
+    
+          
+        function clamp(x, lo, hi){
+          return x < lo ? lo : x > hi ? hi : x;
+        }
+    
+        function click(event, d){
+          delete d.fy;
+          select(this)
+            .attr("fixed", true);
+          simulation.alpha(1).restart();
+        }
+    
+        function dragStart(){
+          select(this)
+            .attr("fixed", true)
+        }
+    
+        function dragged(event, d) {
+          d.value = clamp(d.value-event.dy, 10, 100)
+          simulation.alpha(1).restart();
+        }
+    
+        }, [data])
+    
+        return (
+            <div id="chart-area">
+
+            </div>
+        )
+
+}
+
+export default Lolipop;
