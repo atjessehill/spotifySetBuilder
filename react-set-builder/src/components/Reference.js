6f3e3e3287ca40110d6ef72519367edb50e8b966
Jesse Hill <j_hill14@u.pacific.edu>
2020-11-22 18:34:23 -0500

    Filter dropdown function

--- react-set-builder/src/components/Reference.js
+++ react-set-builder/src/components/Reference.js
@@ -9,6 +9,7 @@ class Reference extends React.Component {
         this.state = {
 
         }
+        this.filterDropDown = this.filterDropDown.bind(this);
 
     }
 
@@ -17,6 +18,40 @@ class Reference extends React.Component {
     componentDidMount(){
     
     }
+
+
+    filterDropDown(event){
+
+        if (event.target.id != 'search-box-id')return
+
+        let filter = event.target.value;
+        let txtValue;
+        let input = document.getElementById('search-box-id');
+        let mainTable = document.getElementById('dropdown-table');
+        let trs = mainTable.getElementsByTagName('tr');
+
+        if(filter.length == 0)document.getElementById('dropdown-block-id').style.display = "none";
+        document.getElementById("dropdown-block-id").style.display = "block";
+
+        for (let i = 0; i < trs.length; i+=3) {
+
+            let tds = trs[i].getElementsByClassName("songs-in-dropdown-list")[0];
+        
+            // console.log(trs[i].id);
+            // console.log(tds);
+        
+            if (tds) {
+              txtValue = tds.textContent || tds.innerText;
+              if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                trs[i].style.display = "";
+              } else {
+                trs[i].style.display = "none";
+              }
+            }
+          }
+
+    }
+
 
 
     render(){
@@ -32,7 +67,8 @@ class Reference extends React.Component {
             <p className="input-headers" style={{"marginTop": "130px"}}><i className="las la-search"></i> Search for a song</p>
 
             <div id="dropdown-group">
-                <input type="search" placeholder="Search for a song..." id="search-box-id" onkeyup="filterDropDown()" class="reference-input-text"/>
+                <input type="search" placeholder="Search for a song..." id="search-box-id" onChange={this.filterDropDown} className="reference-input-text"/> 
+                {/* onkeyup="filterDropDown()" */}
                 <p className="reduntant-text grey1" style={{"marginTop": "-4px"}}>Type at least 3 letters...</p>
 
                 <p className="sub-text" style={{"marginTop": "20px"}}>Genre: <span id="genre-text-id" className="spotify"></span></p>
