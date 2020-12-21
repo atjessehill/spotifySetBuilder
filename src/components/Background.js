eb1fbd1e0ba8c06a7f10e59afa54d1e7b298d992
Jesse Hill <j_hill14@u.pacific.edu>
2020-12-21 13:56:10 -0500

    Added background component

---
+++ src/components/Background.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+class Background extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            from: null,
+            img: null
+        }
+        this.changeBackground = this.changeBackground.bind(this);
+
+    }
+
+    changeBackground(from, img){
+        this.setState({from: from, img: img})
+    }
+
+    render(){
+
+        let background =                 <div id="background-setup" className="album-art">
+        <div id="background-tint"> </div>
+        </div>
+        console.log(this.state)
+        if(this.state.from != null){
+            if (this.state.from == "Playlist"){
+                background = <div id="playlist-background-setup" className="album-art" style={{backgroundImage: `url(${this.state.img})`}}>
+                <div id="background-tint-3"> </div>
+                </div>
+            }
+
+            if (this.state.from == "Home"){
+                background = <div id="background-setup" className="album-art" style={{backgroundImage: `url(../assets/bg-imgs/groove.jpg)`}}>
+                <div id="background-tint"> </div>
+                </div>            
+            }
+
+
+        }
+
+
+
+        return (
+            <div>
+            {background}
+
+            </div>
+
+        )
+    }
+}
+
+export default Background;
